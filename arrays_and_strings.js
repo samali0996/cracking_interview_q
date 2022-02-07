d244d7fd3f323c1a523e19d04ad06670d7f24048
Sam Ali-Mirsalari <Sam.Ali-Mirsalari@ibm.com>
2022-02-07 12:46:54 -0500

    formatting

--- arrays_and_strings.js
+++ arrays_and_strings.js
@@ -1,105 +1,105 @@
 function isUnique(word) {
-    var unique = true
-    if (word.length <= 1) return true
-    word = word.split('')
-    var wordHash = {}
-    word.forEach(letter => {
-        if (!wordHash[letter]) {
-            wordHash[letter] = true
-        } else {
-            unique = false
-        }
-    })
-    return unique
+  var unique = true;
+  if (word.length <= 1) return true;
+  word = word.split('');
+  var wordHash = {};
+  word.forEach((letter) => {
+    if (!wordHash[letter]) {
+      wordHash[letter] = true;
+    } else {
+      unique = false;
+    }
+  });
+  return unique;
 }
 
 function checkPermutation(str1, str2) {
-    if (str1.length !== str2.length) return false
-    str1 = [...str1]
-    str2 = [...str2]
-    str1 = str1.sort((a, b) => a.localeCompare(b))
-    str2 = str2.sort((a, b) => a.localeCompare(b))
-    str1 = str1.join('')
-    str2 = str2.join('')
-    return str1 === str2
+  if (str1.length !== str2.length) return false;
+  str1 = [...str1];
+  str2 = [...str2];
+  str1 = str1.sort((a, b) => a.localeCompare(b));
+  str2 = str2.sort((a, b) => a.localeCompare(b));
+  str1 = str1.join('');
+  str2 = str2.join('');
+  return str1 === str2;
 }
 
 function URLify(inputStr) {
-    const spaceEncode = '%20'
-    inputStr = inputStr.trim().split('')
-    inputStr.forEach((letter, index) => {
-        if (letter === ' ') {
-            inputStr[index] = spaceEncode
-        }
-    })
-    return inputStr.join('')
+  const spaceEncode = '%20';
+  inputStr = inputStr.trim().split('');
+  inputStr.forEach((letter, index) => {
+    if (letter === ' ') {
+      inputStr[index] = spaceEncode;
+    }
+  });
+  return inputStr.join('');
 }
 
 function palPerm(word) {
-    word = word.toLowerCase()
-    if (word.lenght <= 1) return true
-    var isPerm = true
-    word = word.split('')
-    var wordHash = {}
-    var letters = 0
-    word.forEach(letter => {
-        if (letter !== ' ') {
-            letters += 1
-            if (!wordHash[letter]) wordHash[letter] = 1
-            else wordHash[letter] += 1
-        }
-    })
+  word = word.toLowerCase();
+  if (word.lenght <= 1) return true;
+  var isPerm = true;
+  word = word.split('');
+  var wordHash = {};
+  var letters = 0;
+  word.forEach((letter) => {
+    if (letter !== ' ') {
+      letters += 1;
+      if (!wordHash[letter]) wordHash[letter] = 1;
+      else wordHash[letter] += 1;
+    }
+  });
 
-    var canHaveSingle = letters % 2 === 1 ? true : false
-    Object.entries(wordHash).forEach(([letter, occ]) => {
-        if (occ % 2 !== 0) {
-            if (canHaveSingle) canHaveSingle = false
-            else isPerm = false
-        }
-    })
-    return isPerm
+  var canHaveSingle = letters % 2 === 1 ? true : false;
+  Object.entries(wordHash).forEach(([letter, occ]) => {
+    if (occ % 2 !== 0) {
+      if (canHaveSingle) canHaveSingle = false;
+      else isPerm = false;
+    }
+  });
+  return isPerm;
 }
 
 function oneAway(word, editedWord) {
-    var isOneAway = true
-    var diffFound = false
-    if (word === editedWord) return isOneAway
-    if (word.length === editedWord.length) {
-        word.split('').forEach((l, i) => {
-            if (l !== editedWord[i]) {
-                if (!diffFound) diffFound = true
-                else isOneAway = false
-            }
-        })
-    }
-    else if (word.length === editedWord.length + 1 || word.length === editedWord.length - 1) {
-        var smallerWord = word.length < editedWord.length ? word : editedWord
-        var biggerWord = word.length > editedWord.length ? word : editedWord
-        var biggerPointer = 0
-        for (var i = 0; i < smallerWord.length; i++) {
-            smallerLetter = smallerWord[i]
-            biggerLetter = biggerWord[i + biggerPointer]
+  var isOneAway = true;
+  var diffFound = false;
+  if (word === editedWord) return isOneAway;
+  if (word.length === editedWord.length) {
+    word.split('').forEach((l, i) => {
+      if (l !== editedWord[i]) {
+        if (!diffFound) diffFound = true;
+        else isOneAway = false;
+      }
+    });
+  } else if (
+    word.length === editedWord.length + 1 ||
+    word.length === editedWord.length - 1
+  ) {
+    var smallerWord = word.length < editedWord.length ? word : editedWord;
+    var biggerWord = word.length > editedWord.length ? word : editedWord;
+    var biggerPointer = 0;
+    for (var i = 0; i < smallerWord.length; i++) {
+      smallerLetter = smallerWord[i];
+      biggerLetter = biggerWord[i + biggerPointer];
 
-            if (smallerLetter !== biggerLetter) {
-                if (!diffFound) {
-                    diffFound = true
-                    biggerPointer += 1
-                    i -= 1
-                } else {
-                    isOneAway = false
-                }
-            }
+      if (smallerLetter !== biggerLetter) {
+        if (!diffFound) {
+          diffFound = true;
+          biggerPointer += 1;
+          i -= 1;
+        } else {
+          isOneAway = false;
         }
+      }
     }
-    else {
-        isOneAway = false
-    }
-    return isOneAway
+  } else {
+    isOneAway = false;
+  }
+  return isOneAway;
 }
-
 
 /* console.log(isUnique('hh')) */
 /* console.log(checkPermutation('hello', 'elloh')) */
 /* console.log(URLify('Mr John Smith    ')) */
 // console.log(palPerm('areare'))
-console.log(oneAway('v', 'ba'))
+console.log(oneAway('v', 'ba'));
--- linked_lists.js
+++ linked_lists.js
@@ -1,75 +1,71 @@
 class Node {
     constructor(data) {
-        this.data = data
-        this.next = null
+        this.data = data;
+        this.next = null;
     }
     appendToTail(d) {
-        var end = new Node(d)
-        var n = this
+        var end = new Node(d);
+        var n = this;
         while (n.next != null) {
-            n = n.next
+            n = n.next;
         }
-        n.next = end
+        n.next = end;
     }
     printLinkedList() {
-        var string = ''
-        var n = this
+        var string = '';
+        var n = this;
         while (n != null) {
-            string += `${n.data}-->`
-            n = n.next
+            string += `${n.data}-->`;
+            n = n.next;
         }
-        console.log(string)
+        console.log(string);
     }
 }
 
 function remDepsHash(head) {
-    var dataHash = {}
-    var n = head
-    var prev = null
+    var dataHash = {};
+    var n = head;
+    var prev = null;
     while (n != null) {
         if (!dataHash[n.data]) {
-            dataHash[n.data] = true
-            prev = n
+            dataHash[n.data] = true;
+            prev = n;
         } else {
-            prev.next = n.next
+            prev.next = n.next;
         }
-        n = n.next
+        n = n.next;
     }
 }
 
 function remDeps(toDelete, head) {
-    var n = head.next
-    var prev = head
+    var n = head.next;
+    var prev = head;
     while (n !== null) {
         if (n.data === toDelete) {
-            prev.next = n.next
+            prev.next = n.next;
         } else {
-            prev = n
+            prev = n;
         }
-        n = n.next
+        n = n.next;
     }
 }
 
 function remDepsNoHash(head) {
-    var n = head
+    var n = head;
     while (n.next !== null) {
-        remDeps(n.data, n)
-        n = n.next
+        remDeps(n.data, n);
+        n = n.next;
     }
 }
 
-var head = new Node(2)
-head.appendToTail(3)
-head.appendToTail(4)
-head.appendToTail(2)
-head.appendToTail(5)
-head.appendToTail(2)
-head.appendToTail(3)
-head.printLinkedList()
+var head = new Node(2);
+head.appendToTail(3);
+head.appendToTail(4);
+head.appendToTail(2);
+head.appendToTail(5);
+head.appendToTail(2);
+head.appendToTail(3);
+head.printLinkedList();
 // remAllDeps(head)
-remDepsNoHash(head)
-head.printLinkedList()
-
-
-
-
+remDepsNoHash(head);
+head.printLinkedList();
